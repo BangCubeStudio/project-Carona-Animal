51337149a32b735c0ca83270475da881a19cc1c5
marinsfabio <fabiomarinspro@hotmail.com>
2023-05-23 11:45:28 -0300

    fabio mexeu aqui

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,30 +1,30 @@
-import React from 'react'
+import { useState } from 'react'
 import classes from '../components/Hero.module.css'
 import buttonImg from '../assets/pet-taxi.png'
 
 const Hero = () => {
-    // const RedWhats = (
-        
-    // )
-    
-    
-    return (
-        <div className={classes.Hero}>
-            <section className={classes.container}>
-                <h1 className={classes.title}>Carona Animal</h1>
-                <p className={classes.textP}>Onde há carona animal há amor!</p>  
-                <div className={classes.divButton}>
-                    {/* <button className={classes.button} onClick={RedWhats}>
-                    </button> */}
-                    <img className={classes.butImg} src={buttonImg} alt="" />
-                    
-                
-                </div>
-                 
-            </section>
-                  
+  const [img, setImg] = useState(false)
+
+  return (
+    <div className={classes.Hero}>
+      <section className={classes.container}>
+        <h1 className={classes.title}>Carona Animal</h1>
+        <p className={classes.textP}>Onde há carona animal há amor!</p>
+        <div className={classes.divButton}>
+          
+          <button className={`${classes.button} ${img ? setImg : ""}`}
+            onMouseEnter={() => setImg(true)}
+            onMouseLeave={() => setImg(false)}>
+            saiba mais
+          </button>
+          {img && (
+            <img src={buttonImg} alt="" />
+          )}
         </div>
-    )
-} 
+      </section>
+
+    </div>
+  )
+}
 
 export default Hero
