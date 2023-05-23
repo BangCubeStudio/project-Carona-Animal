28c9317ae50b47965590b24df19fc60d07778dac
Mac Mini <macmini@Mac-mini-de-Mac-2.local>
2023-05-23 17:49:32 -0300

    feat(responsivo mobile)Adicionando responsividade de desktop para mobile

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -3,37 +3,22 @@ import classes from '../components/Hero.module.css'
 import buttonImg from '../assets/pet-taxi.png'
 
 const Hero = () => {
-  const [img, setImg] = useState(false)
-
+  const [img, setImg] = useState(false)  
+  
   return (
     <div className={classes.Hero}>
       <section className={classes.container}>
         <h1 className={classes.title}>Carona Animal</h1>
         <p className={classes.textP}>Onde há carona animal há amor!</p>
         <div onMouseLeave={() => setImg(false)}  className={classes.divButton}>
-
         {!img? (<button className={`${classes.button} ${img ? setImg : ""}` }
             onMouseEnter={() => setImg(true) }
-            
             >
             Saiba mais
           </button>) :
           (
             <img src={buttonImg} alt="" />
           )}
-        
-
-
-
-          {/* <button className={`${classes.button} ${img ? setImg : ""}` }
-            onMouseEnter={() => setImg(true) }
-            onMouseLeave={() => setImg(false)} 
-            >
-            saiba mais
-          </button>
-          {img && (
-            <img src={buttonImg} alt="" />
-          )} */}
         </div>
       </section>
 
