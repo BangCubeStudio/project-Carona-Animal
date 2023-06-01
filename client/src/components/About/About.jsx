17a4ab0d3e330d5fc64cf3d078bc7b0f1bdf462f
AndressaGabriele <andressa.chagas79@gmail.com>
2023-06-01 17:25:24 -0300

    refactor Hero, About

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -3,19 +3,6 @@ import classes from "../About/About.module.css";
 const About = () => {
   return (
     <section className={classes.container} id="About">
-      {/* <h1 className={classes.h1}>Serviços</h1> */}
-      {/* <div className={classes.about}> */}
-      {/* <p className={classes.aboutP}>
-          Meu nome é Pamela tenho 37 anos, sou muito sonhadora e com muita
-          garra, tenho um amor incondicional por animais. E com esse amor surgiu
-          a ideia de desenvolver um aplicativo de taxi pet para aquelas pessoas
-          que enfrentam desafios para levar o seu pet em um passeio, viajens,
-          veterinário, ou ate mesmo em pet shop, e amam e querem o melhor para
-          eles, tendo preservação a segurança e o conforto acima de tudo. E é
-          então que surge o app carona animal vindo para facilitar a sua vida
-          podendo ficar cada vez mais proximo do seu animal de extimação.
-        </p> */}
-      {/* <h2 className={classes.services}>Serviços prestados</h2> */}
       <div className={classes.testContainer}>
         <div className={classes.test}>
           <h2 className={classes.services}>Pet Walker</h2>
@@ -53,7 +40,6 @@ const About = () => {
           </li>
         </div>
       </div>
-      {/* </div> */}
     </section>
   );
 };
