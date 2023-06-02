86d2988453b790f7c99d91df40d7b7993f7ece10
AndressaGabriele <andressa.chagas79@gmail.com>
2023-06-01 21:11:39 -0300

    Refactor

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -9,9 +9,9 @@ const About = () => {
 
           <li className={classes.servicesLi}>
             {" "}
-            Serviço de passeadores de aumigos
+            Serviço de passeadores para o seu aumigo
           </li>
-          <li className={classes.servicesLi}> Excelentes profissionais que amam os aumigos </li>
+          <li className={classes.servicesLi}> Excelentes profissionais que amam o que faz</li>
           <li className={classes.servicesLi}> Valores acessíveis </li>
           <li className={classes.servicesLi}> Reduz o estress e o acalma </li>
           <li className={classes.servicesLi}> Melhorias de sistemas imunológicos </li>
